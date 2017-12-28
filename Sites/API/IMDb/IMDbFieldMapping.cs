90ed07e476ffe8c19dc2632737bcf3c5d90c7221
Cogito\O <lamothe@gmail.com>
2017-12-28 16:44:34 -0500

    Fix IMDB header change for "your rating" column.

--- Sites/API/IMDb/IMDbFieldMapping.cs
+++ Sites/API/IMDb/IMDbFieldMapping.cs
@@ -7,7 +7,7 @@
         public const string cCreated = "created";
         public const string cTitle = "Title";
         public const string cType = "Title type";
-        public const string cRating = "You rated";
+        public const string cRating = "Your Rating";
         public const string cYear = "Year";
         public const string cReleaseDate = "Release Date (month/day/year)";
         public const string cProvider = "Provider";
