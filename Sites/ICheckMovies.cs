d0f8c2ce7b02581a6a0b42ae45b83309a121b057
damienhaynes <damien.haynes@gmail.com>
2017-12-30 12:47:50 +1100

    Added batch import for iCheckMovies.

--- Sites/ICheckMovies.cs
+++ Sites/ICheckMovies.cs
@@ -70,25 +70,46 @@ namespace TraktRater.Sites
         {
             UIUtils.UpdateStatus("Updating Trakt watched history with movies from ICheckMovies.");
 
-            var watchedToSync = new TraktMovieWatchedSync()
+            if (watchedMovies.Count() > 0)
             {
-                Movies = watchedMovies
-            };
+                int pageSize = AppSettings.BatchSize;
+                int pages = (int)System.Math.Ceiling((double)watchedMovies.Count() / pageSize);
+                for (int i = 0; i < pages; i++)
+                {
+                    UIUtils.UpdateStatus("Importing page {0}/{1} ICheckMovies watched history...", i + 1, pages);
 
-            var addToWatchedHistoryResponse = TraktAPI.TraktAPI.AddMoviesToWatchedHistory(watchedToSync);
-            HandleResponse(addToWatchedHistoryResponse);
+                    var watchedToSync = new TraktMovieWatchedSync()
+                    {
+                        Movies = watchedMovies.Skip(i * pageSize).Take(pageSize).ToList()
+                    };
+
+                    var addToWatchedHistoryResponse = TraktAPI.TraktAPI.AddMoviesToWatchedHistory(watchedToSync);
+                    HandleResponse(addToWatchedHistoryResponse);
+                }
+            }
         }
 
         private void AddMoviesToWatchlist(IEnumerable<ICheckMoviesListItem> watchListMovies)
         {
             UIUtils.UpdateStatus("Updating Trakt watchlist with movies from ICheckMovies.");
-            var watchlistToSync = new TraktMovieSync()
-            {
-                Movies = watchListMovies.Select(icm => icm.ToTraktMovie()).ToList()
-            };
 
-            var addToWatchlistResponse = TraktAPI.TraktAPI.AddMoviesToWatchlist(watchlistToSync);
-            HandleResponse(addToWatchlistResponse);
+            if (watchListMovies.Count() > 0)
+            {
+                int pageSize = AppSettings.BatchSize;
+                int pages = (int)System.Math.Ceiling((double)watchListMovies.Count() / pageSize);
+                for (int i = 0; i < pages; i++)
+                {
+                    UIUtils.UpdateStatus("Importing page {0}/{1} ICheckMovies watchlist...", i + 1, pages);
+
+                    var watchlistToSync = new TraktMovieSync()
+                    {
+                        Movies = watchListMovies.Skip(i * pageSize).Take(pageSize).Select(icm => icm.ToTraktMovie()).ToList()
+                    };
+
+                    var addToWatchlistResponse = TraktAPI.TraktAPI.AddMoviesToWatchlist(watchlistToSync);
+                    HandleResponse(addToWatchlistResponse);
+                }
+            }
         }
 
         private static void HandleResponse(TraktSyncResponse addToWatchlistResponse)
