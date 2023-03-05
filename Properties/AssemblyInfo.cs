ef4c6989085b296b5162a9748c30dc03abc52004
Damien Haynes <damien.haynes@gmail.com>
2023-03-05 15:14:17 +1100

    fix(trakt): added simple rate limit handling (#149)
    Resolves: #148
    
    Co-authored-by: Damien Haynes <dhaynes@tradevela.com>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TraktRater")]
-[assembly: AssemblyCopyright("Copyright © 2012-2022")]
+[assembly: AssemblyCopyright("Copyright © 2012-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.12.0")]
-[assembly: AssemblyFileVersion("2.3.12.0")]
+[assembly: AssemblyVersion("2.3.13.0")]
+[assembly: AssemblyFileVersion("2.3.13.0")]
